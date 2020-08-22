9e5a7781c470d356e02ebf1ceaee39402a2077d6
pradhuman1 <pradhumansinghbaid@gmail.com>
2020-08-22 20:58:53 +0530

    Code completed

--- findWord.bash
+++ findWord.bash
@@ -3,13 +3,28 @@
 echo "enter directory you want to find a word : "
 read dir
 cd ./"$dir"
-for file in `ls`
+echo "enter word you want to find : "
+read word
+totalCount=0
+flag=0
+for file in `find ./| grep .txt`
 do
-echo "$file"
-echo
-while IFS= read -r line
-do
-  echo "$line"
-done < "$file"
-
+  flag=0
+  while IFS= read -r line
+  do
+    for word1 in $line
+    do 
+      if [ $word1 == $word ]
+      then
+        totalCount="$[totalCount+1]"
+        flag=1
+      fi
+    done
+  done < "$file"
+  if [ $flag == 1 ]
+  then
+    echo "Word "$word" is found in "$file" " 
+  fi
 done
+echo "Total number of occurance of $word is : "$totalCount
+
