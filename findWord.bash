a3067e776735184221bbfd0bd3eac962366dc16f
pradhuman1 <pradhumansinghbaid@gmail.com>
2020-08-22 21:35:08 +0530

    Occurence is now file wise also

--- findWord.bash
+++ findWord.bash
@@ -6,10 +6,11 @@ cd ./"$dir"
 echo "enter word you want to find : "
 read word
 totalCount=0
-flag=0
+count=0
 for file in `find ./| grep .txt`
 do
-  flag=0
+  count=0
+  
   while IFS= read -r line
   do
     for word1 in $line
@@ -17,14 +18,11 @@ do
       if [ $word1 == $word ]
       then
         totalCount="$[totalCount+1]"
-        flag=1
+        count="$[count+1]"
       fi
     done
   done < "$file"
-  if [ $flag == 1 ]
-  then
-    echo "Word "$word" is found in "$file" " 
-  fi
+  echo "Number of occurance of Word \" "$word" \" found in  FILE : "$file" is : $count"     
 done
 echo "Total number of occurance of $word is : "$totalCount
 
