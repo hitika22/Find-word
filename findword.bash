8c9805d4ecce8ecb40c918bc01269a0976343cba
Shyam Tayal <shyam.tayal@hotmail.com>
2020-08-25 20:22:19 +0530

    More testing files with subdirs and minor tweeks in the bash file

--- findword.bash
+++ findword.bash
@@ -6,11 +6,11 @@ cd ./"$dir"
 echo -n "enter word you want to find : "
 read word
 totalCount=0
-flag=0
+count=0
 for file in `find ./| grep .txt`
 do
-  flag=0
-  sepCount=0
+  count=0
+
   while IFS= read -r line
   do
     for word1 in $line
@@ -18,14 +18,10 @@ do
       if [ $word1 == $word ]
       then
         totalCount="$[totalCount+1]"
-        sepCount="$[sepCount+1]"
-        flag=1
+        count="$[count+1]"
       fi
     done
   done < "$file"
-  if [ $flag == 1 ]
-  then
-    echo "Total number of occurance of $word in "$file" : "$sepCount
-  fi
+  echo "Number of occurance of \""$word"\" found in  FILE : "$file" is : $count"
 done
-echo "Total number of occurance of $word in $dir : "$totalCount
+echo "Total number of occurance of \""$word"\" in the directory \""$dir"\" is : "$totalCount
