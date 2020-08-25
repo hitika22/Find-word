0988b6194d23774eb0b71c5c3309e5fc8ebde7c4
pradhuman1 <pradhumansinghbaid@gmail.com>
2020-08-25 13:42:35 +0530

    filename with space - bug fixed

--- findWord.bash
+++ findWord.bash
@@ -7,21 +7,24 @@ echo "enter word you want to find : "
 read word
 totalCount=0
 count=0
-for file in `find ./| grep .txt`
+IFS=$'\n'
+for file in `find ./| grep .txt$`
 do
   count=0
-  
+  unset IFS
   while IFS= read -r line
   do
     for word1 in $line
     do 
       if [ $word1 == $word ]
       then
+        
         totalCount="$[totalCount+1]"
         count="$[count+1]"
       fi
     done
   done < "$file"
+  IFS=$'\n'
   echo "Number of occurance of Word \" "$word" \" found in  FILE : "$file" is : $count"     
 done
 echo "Total number of occurance of $word is : "$totalCount
