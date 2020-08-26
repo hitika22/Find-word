473adf633b427261bae28a3590ddad68dd0c8ef0
Abhijeet Sonkar <abhijeet.iiita.iib@gmail.com>
2020-08-26 11:05:17 +0530

    Used alternative for realpath

--- alternate.bash
+++ alternate.bash
@@ -8,15 +8,22 @@ echo "enter word you want to find : "
 read word
 totalCount=0
 
-for file in `find ./| grep txt$`
+for file in `find ./| grep "\\.txt$"`
 do
-    abspath=`realpath $file`
-    filename=`basename $abspath`
+  
+    file="${file:1}"
+    abspath="$dir$file"
+    filename=${abspath##*/}
+    if [ -d "$abspath" ];                                   
+    then
+        continue                                         
+    fi  
     for i in `grep -o -w "$word" $abspath | wc -l`
     do
         
         echo "Number of occurance of Word \" "$word" \" found in  FILE : "$filename" is : $i"   
         totalCount="$[totalCount+i]"
     done
+     
 done
 echo $totalCount
